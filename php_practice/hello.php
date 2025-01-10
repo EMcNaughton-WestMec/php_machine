2c7ff25d7042e395821e7a94e7d20dd1dc95612e
EMcNaughton-WestMec <142362842+EMcNaughton-WestMec@users.noreply.github.com>
2025-01-10 00:29:42 +0000

    1/9/25

---
+++ php_practice/hello.php
@@ -0,0 +1,9 @@
+<?php
+
+$myName = "Ethan";
+$myFriend = "Ben";
+echo "Hello, World!\n";
+echo "My name is $myName\n";
+echo "my friends name is $myFriend\n";
+echo 'Hello ' . $myFriend . ' my name is '. $myName . "\n";
+
